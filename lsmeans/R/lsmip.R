d9880d3bd54bf0d3af0d4e24e5ab5ebb0ce6dbe5
rlenth <rlenth@7354427b-d295-4c4f-9a55-d8da99b2380c>
2013-03-31 22:40:55 +0000

    lsmeans: preliminary code for lsmip (interaction plots)

---
+++ lsmeans/R/lsmip.R
@@ -0,0 +1,27 @@
+# lsmip code - interaction plots
+
+# object - a model object supported by lsmeans
+# formula - a formula of the form  x.factors ~ trace.factors | panel.factors
+lsmip = function(object, formula, se.bars = TRUE, ...) {
+    if (!require("lattice"))
+        stop("This function requires the 'lattice' package be installed.")
+    if (length(formula) < 3)
+        stop("'formula' must be two-sided, e.g. x.factor ~ trace.factor")
+    allvars = all.vars(formula)
+    form = as.formula(paste("~", paste(allvars, collapse = "+")))
+    lsms = lsmeans(object, form)[[1]]
+    xvars = all.vars(formula[[2]])
+    lsms$xvar = factor(do.call(paste, lsms[xvars]))
+    lsms = lsms[order(lsms$xvar), ]
+    rhs = strsplit(as.character(formula[3]), "\\|")[[1]]
+    tvars = all.vars(as.formula(paste("~", rhs[[1]])))
+    lsms$tvar = factor(do.call(paste, lsms[tvars]))
+    plotform = lsmean ~ xvar
+    if (length(rhs) > 1) {
+        byvars = all.vars(as.formula(paste("~", rhs[[2]])))
+        lsms$byvar = factor(do.call(paste, lsms[byvars]))
+        plotform = as.formula("lsmean ~ xvar | byvar")
+    }
+    print(xyplot(plotform, groups=~ tvar, type="o", data=lsms))
+    invisible(lsms)
+}
