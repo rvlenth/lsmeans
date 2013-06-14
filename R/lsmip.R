14beae302aa75f45a1a57e4fd3041950b7f89fd5
Russell V. Lenth <russell-lenth@uiowa.edu>
2013-06-14 16:42:53 +0000

    lsmeans -- Documentation of lsmip

--- R/lsmip.R
+++ R/lsmip.R
@@ -7,9 +7,15 @@ lsmip = function(object, formula, pch=c(1,2,6,7,9,10,15:20), lty=1, col=NULL, ..
         stop("This function requires the 'lattice' package be installed.")
     if (length(formula) < 3)
         stop("'formula' must be two-sided, e.g. trace.factor ~ x.factor")
-    allvars = all.vars(formula)
-    form = as.formula(paste("~", paste(allvars, collapse = "+")))
-    lsms = lsmeans(object, form)[[1]]
+    ylab = "Least-squares mean"
+    if (inherits(object,"lsm"))
+        lsms = object[[1]]
+    else {
+        allvars = all.vars(formula)
+        form = as.formula(paste("~", paste(allvars, collapse = "+")))
+        lsms = lsmeans(object, form)[[1]]
+        ylab = paste(ylab, "of", formula(object)[[2]])
+    }
     tvars = all.vars(formula[[2]])
     lsms$tvar = factor(do.call(paste, lsms[tvars]))
     rhs = strsplit(as.character(formula[3]), "\\|")[[1]]
@@ -36,10 +42,12 @@ lsmip = function(object, formula, pch=c(1,2,6,7,9,10,15:20), lty=1, col=NULL, ..
     if (!is.null(col)) TP$superpose.symbol$col = TP$superpose.line$col = col
     trellis.par.set(TP)
     grobj = xyplot(plotform, groups= tvar, data=lsms, 
-        xlab = paste("Levels of", paste(xvars, collapse=" * ")),
-                   ylab = paste("Least-squares mean of", formula(object)[[2]]),
-        strip = my.strip,
-        auto.key = my.key(tvars), type=c("p","l"), ... )
+                   xlab = paste("Levels of", paste(xvars, collapse=" * ")),
+                   ylab = ylab,
+                   strip = my.strip,
+                   auto.key = my.key(tvars), 
+                   type=c("p","l"), 
+                   ... )
     print(grobj)
     trellis.par.set(TP.orig)
     invisible(lsms)
