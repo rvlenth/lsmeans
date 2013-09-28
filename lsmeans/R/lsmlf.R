b3c1e8b3481afdaeaadab52de8aa8f7033fa9df4
rlenth <rlenth@7354427b-d295-4c4f-9a55-d8da99b2380c>
2013-09-28 16:08:08 +0000

    lsmeans - dependency fixes, I think

--- lsmeans/R/lsmlf.R
+++ lsmeans/R/lsmlf.R
@@ -12,18 +12,23 @@ lsm <- function(...) {
 }
 
 # here is S3 method for class "lsmlf" in glht
-glht.lsmlf <- function(model, linfct, ...) {
-# linfct has the arguments to pass to lsmeans. We just need to fill it out and call
-    linfct$object <- model
-    linfct$lf <- TRUE
+if (require(multcomp)) {
     
-    # In this implementation, we'll NOT accommodate lists
-    spec <- linfct$specs
-    if (is.list(spec)) linfct$specs <- spec[[1]]
+    glht.lsmlf <- function(model, linfct, ...) {
+        # linfct has the arguments to pass to lsmeans. We just need to fill it out and call
+        linfct$object <- model
+        linfct$lf <- TRUE
+        
+        # In this implementation, we'll NOT accommodate lists
+        spec <- linfct$specs
+        if (is.list(spec)) linfct$specs <- spec[[1]]
+        
+        # Get the linear function
+        lf <- do.call("lsmeans", linfct)
+        
+        # Just use the last result - will be linfct for lsmeans if no rhs, else for the contrasts
+        glht(model, linfct = lf[[length(lf)]], ...)
+    }
     
-    # Get the linear function
-    lf <- do.call("lsmeans", linfct)
-    
-    # Just use the last result - will be linfct for lsmeans if no rhs, else for the contrasts
-    glht(model, linfct = lf[[length(lf)]], ...)
-}
+}# [if(require(multcomp))]
+
