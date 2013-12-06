d86ab873b17d230258d4fb6892f56c3e0ed693f1
Russell V. Lenth <russell-lenth@uiowa.edu>
2013-12-06 21:04:13 +0000

    lsmeans -- beginnings of a generic for recovering data

--- R/recover.data.R
+++ R/recover.data.R
@@ -1,18 +1,5 @@
 # Recover data routines
 
-# initial broader attempt - not all that successful
-recover.data = function(call) {
-    of.interest = match(c("formula","data","subset","na.action"), names(call), 0)
-    of.interest = of.interest[of.interest > 0]
-    args = as.list(call[of.interest])
-    vars = all.vars(args$formula)
-    form = as.formula(paste("~", paste(vars, collapse="+")))
-    args$formula = update.formula(args$formula, form)
-    do.call("model.frame", args)
-}
-
-
-
 # The following function returns a data.frame with the variables
 # from the given object. 
 # It also adds attributes "predictors" and "responses"
@@ -20,7 +7,7 @@ recover.data = function(call) {
 
 # Currently this works correctly for obj of class "lm",
 # examples warp.data, warp.log, warp.with; and warp.att in data attached
-rd = function(obj) {
+.old.rd = function(obj) {
     fcall <- obj$call
     m <- match(c("formula", "data", "subset", "weights", 
                  "na.action", "offset"), names(fcall), 0L)
@@ -34,7 +21,7 @@ rd = function(obj) {
     # Put one var on left - keeps out lhs transformations
     form <- as.formula(paste(vars[1], "~", paste(vars[-1], collapse="+")))
     fcall$formula <- update(trms, form)
-    env <- environment(obj$terms)
+    env <- environment(trms)
     if (is.null(env)) 
         env <- parent.frame()
     tbl <- eval(fcall, env, parent.frame())
@@ -42,4 +29,42 @@ rd = function(obj) {
     attr(tbl, "predictors") = all.vars(delete.response(trms))
     attr(tbl, "responses") = setdiff(vars, attr(tbl, "predictors"))
     tbl
+}
+
+# Attempt at general function
+.recover.data <- function(fcall, xlevels, trms) {
+    m <- match(c("formula", "data", "subset", "weights", 
+                 "na.action", "offset"), names(fcall), 0L)
+    fcall <- fcall[c(1L, m)]
+    fcall$drop.unused.levels <- TRUE
+    fcall[[1L]] <- as.name("model.frame")
+    fcall$xlev <- xlevels
+    vars <- all.vars(trms) # (length will always be >= 2)
+    # Put one var on left - keeps out lhs transformations
+    form <- as.formula(paste(vars[1], "~", paste(vars[-1], collapse="+")))
+    fcall$formula <- update(trms, form)
+    env <- environment(trms)
+    if (is.null(env)) 
+        env <- parent.frame()
+    tbl <- eval(fcall, env, parent.frame())
+    attr(tbl, "terms") = NULL
+    attr(tbl, "predictors") = all.vars(delete.response(trms))
+    attr(tbl, "responses") = setdiff(vars, attr(tbl, "predictors"))
+    tbl
+}
+
+rd = function(object, ...)
+    UseMethod("rd")
+
+rd.lm = function(lmobj) {
+    fcall = lmobj$call
+    xlev = fcall$xlevels
+    .recover.data(fcall, xlev, terms(lmobj))
+}
+
+rd.merMod = function(object) {
+    fcall = object@call
+    xlev = fcall$xlevels
+    .recover.data(fcall, xlev, terms(object))
+    
 }
