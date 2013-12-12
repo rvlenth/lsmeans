af3d3e16d590fbb80f089d37cde05171fa27a25c
Russell V. Lenth <russell-lenth@uiowa.edu>
2013-12-12 21:44:23 +0000

    lsmeans - more on ref.grid and new way of handling cov.reduce

--- R/recover.data.R
+++ R/recover.data.R
@@ -1,19 +1,22 @@
 # Recover data routines
 
+# The following function returns a data.frame with the variables
+# from the given object. 
+# It also adds attributes "predictors" and "responses"
+# This function is adapted from model.frame.lm
 
-# Workhorse for methods
-#! Note: Seems like we don't need xlev, and if we use it, we
-#! can get warning messages for models like y ~ factor(dose)
-#! Moreover, looking at model.frame code, we won't get to 
-#! drop.unused.levels part if xlev is NOT null!
-.rd.wh <- function(fcall, xlev, trms) {
+# Currently this works correctly for obj of class "lm",
+# examples warp.data, warp.log, warp.with; and warp.att in data attached
+.old.rd = function(obj) {
+    fcall <- obj$call
     m <- match(c("formula", "data", "subset", "weights", 
                  "na.action", "offset"), names(fcall), 0L)
     fcall <- fcall[c(1L, m)]
     fcall$drop.unused.levels <- TRUE
     fcall[[1L]] <- as.name("model.frame")
-    fcall$xlev <- NULL ##OMIT## xlev
-    fcall$na.action <- na.omit
+    fcall$xlev <- obj$xlevels
+    # replace formula by list of vars w/out function calls
+    trms <- terms(obj)
     vars <- all.vars(trms) # (length will always be >= 2)
     # Put one var on left - keeps out lhs transformations
     form <- as.formula(paste(vars[1], "~", paste(vars[-1], collapse="+")))
@@ -22,59 +25,48 @@
     if (is.null(env)) 
         env <- parent.frame()
     tbl <- eval(fcall, env, parent.frame())
-    attr(tbl, "terms") = trms
+    attr(tbl, "terms") = NULL
     attr(tbl, "predictors") = all.vars(delete.response(trms))
     attr(tbl, "responses") = setdiff(vars, attr(tbl, "predictors"))
     tbl
 }
 
-### S3 Methods...
-# All .recover.data methods will return a data.frame with at least these 
-# additional attrs:
-#   attr(, "terms")      - terms component of object
-#   attr(, "responses")  - names of response variable
-#   attr(, "predictors") - names of predictors
-
-# generic
-.recover.data = function(object, ...)
-    UseMethod(".recover.data")
-
-# default -- used for classes we don't know about
-.recover.data.default = function(object, ...)
-    stop(paste("Can't handle a model of class", class(object)[1]))
-    
-# stats...
-.recover.data.lm = function(object) {
-    fcall = object$call
-    xlev = object$xlevels
-    .rd.wh(fcall, xlev, terms(object))
-}
-# (also aov is extension of lm when no Error() in model)
-
-# MASS
-.recover.data.lqs = function(object)
-    .recover.data.lm(object)
-# (also rlm is extension of lm)
-
-# nlme ...
-.recover.data.lme = function(object)
-    .recover.data.lm(object)
-
-.recover.data.gls = function(object) {
-    fcall = object$call
-    xlev = object$xlevels
-    .rd.wh(fcall, xlev, getCovariateFormula(object))
+# Attempt at general function
+.recover.data <- function(fcall, xlevels, trms) {
+    m <- match(c("formula", "data", "subset", "weights", 
+                 "na.action", "offset"), names(fcall), 0L)
+    fcall <- fcall[c(1L, m)]
+    fcall$drop.unused.levels <- TRUE
+    fcall[[1L]] <- as.name("model.frame")
+    fcall$xlev <- xlevels
+    vars <- all.vars(trms) # (length will always be >= 2)
+    # Put one var on left - keeps out lhs transformations
+    form <- as.formula(paste(vars[1], "~", paste(vars[-1], collapse="+")))
+    fcall$formula <- update(trms, form)
+    env <- environment(trms)
+    if (is.null(env)) 
+        env <- parent.frame()
+    tbl <- eval(fcall, env, parent.frame())
+    attr(tbl, "terms") = NULL
+    attr(tbl, "predictors") = all.vars(delete.response(trms))
+    attr(tbl, "responses") = setdiff(vars, attr(tbl, "predictors"))
+    tbl
 }
 
-# lme4 ...
-# If it turns out we need xleves after all, we'll need to fix this
-.recover.data.merMod = function(object) {
+rd = function(object, ...)
+    UseMethod("rd")
+
+rd.lm = function(lmobj) {
+    fcall = lmobj$call
+    xlev = fcall$xlevels
+    .recover.data(fcall, xlev, terms(lmobj))
+}
+
+rd.merMod = function(object) {
+    if(!isLMM(object) && !isGLMM(object)) 
+        stop("Can't handle a nonlinear mixed model")
     fcall = object@call
-    xlev = NULL ###object$xlevels
-    .rd.wh(fcall, xlev, terms(object))
+    xlev = fcall$xlevels
+    .recover.data(fcall, xlev, terms(object))
+    
 }
-
-# lme4.0 if I can get it and test it
-.recover.data.mer = function(object)
-    .recover.data.merMod(object)
-
--- R/ref.grid.R
+++ R/ref.grid.R
@@ -1,12 +1,18 @@
 # Reference grid code
 
 # S4 class definition:
-setClass("ref.grid", representation(
+setClass("ref.grid", representation (
     predictors = "character",
     responses = "character",
     grid = "data.frame", 
     levels = "list",
-    matlevs = "list"
+    matlevs = "list",
+    X = "matrix",
+    bhat = "numeric",
+    nbasis = "matrix",
+    V = "matrix",
+    ddfm = "function",
+    misc = "list"
 ))
 
 setMethod("show", "ref.grid", function(object) {
@@ -28,7 +34,13 @@ setMethod("show", "ref.grid", function(object) {
     }
 })
 
-ref.grid <- function(object, at, cov.reduce = function(x, name) mean(x)) {
+# Change to cov.reduce specification: can be...
+#     a function: is applied to all covariates
+#     named list of functions: applied to those covariates (else mean is used)
+#     TRUE - same as mean
+#     FALSE - same as function(x) sort(unique(x))
+
+ref.grid <- function(object, at, cov.reduce = mean) {
     # recover the data
     data = .recover.data (object)
     
@@ -36,11 +48,30 @@ ref.grid <- function(object, at, cov.reduce = function(x, name) mean(x)) {
     anm = all.names(attr(data, "terms"))    
     coerced = anm[1 + grep("factor|ordered", anm)]
     
-    # initialize empty lists
-    ref.levels = matpred = list()
+    # convenience functions
+    sort.unique = function(x) sort(unique(x))
+    if(is.logical(cov.reduce)) 
+        if(cov.reduce[1]) cov.reduce = mean
+        else              cov.reduce = sort.unique
+    cr = function(x, nm) {
+        if (is.function(cov.reduce))
+            cov.reduce(x)
+        else if (!is.null(cov.reduce[[nm]]))
+            cov.reduce[[nm]](x)
+        else
+            mean(x)
+    }
     
-    for (nm in attr(data, "responses"))
-        ref.levels[[nm]] = mean(data[[nm]])
+    # initialize empty lists
+    ref.levels = matlevs = xlev = list()
+    
+    for (nm in attr(data, "responses")) {
+        y = data[[nm]]
+        if (is.matrix(y))
+            matlevs[[nm]] = apply(y, 2, mean)
+        else
+            ref.levels[[nm]] = mean(y)
+    }
     
     for (nm in attr(data, "predictors")) {
         x = data[[nm]]
@@ -49,33 +80,38 @@ ref.grid <- function(object, at, cov.reduce = function(x, name) mean(x)) {
             ref.levels[[nm]] = at[[nm]]
         # factors not in 'at'
         else if (is.factor(x))
-                ref.levels[[nm]] = levels(x)
+                xlev[[nm]] = ref.levels[[nm]] = levels(x)
         # matrices
         else if (is.matrix(x)) {
             # Matrices -- reduce columns thereof, but don't add to baselevs
-            matpred[[nm]] = if (!is.logical(cov.reduce)) apply(x, 2, cov.reduce, nm)
-                           else apply(x, 2, mean)
+            matlevs[[nm]] = apply(x, 2, cr, nm)
+            # if cov.reduce returns a vector, average its columns
+            if (is.matrix(matlevs[[nm]]))
+                matlevs[[nm]] = apply(matlevs[[nm]], 2, mean)
         }
         # covariate not mentioned in 'at'
         else {
-            # cov.reduce == FALSE or
             # single numeric pred but coerced to a factor - use unique values
-            if ((is.logical(cov.reduce) && !cov.reduce) || length(grep(nm, coerced)) > 0)             
-                ref.levels[[nm]] = sort(unique(x))
+            if (length(grep(nm, coerced)) > 0)             
+                ref.levels[[nm]] = sort.unique(x)
             
             # Ordinary covariates - summarize
             else 
-                ref.levels[[nm]] = cov.reduce(x, nm)
+                ref.levels[[nm]] = cr(x, nm)
         }
     }
     
     # Now create the reference grid
     grid = do.call(expand.grid, ref.levels)
     # add any matrices
-    for (nm in names(matpred))
-        grid[[nm]] = matrix(rep(matpred[[nm]], each=nrow(grid)), nrow=nrow(grid))
+    for (nm in names(matlevs))
+        grid[[nm]] = matrix(rep(matlevs[[nm]], each=nrow(grid)), nrow=nrow(grid))
+    
+    basis = lsm.basis(object, attr(data, "terms"), xlev, grid)
     
     new ("ref.grid", 
          predictors = attr(data, "predictors"), responses = attr(data, "responses"),
-         grid = grid, levels = ref.levels, matlevs = matpred)
+         grid = grid, levels = ref.levels, matlevs = matlevs,
+         X = basis$X, bhat = basis$bhat, nbasis = basis$nbasis, V = basis$V,
+         ddfm = basis$ddfm, misc = basis$misc)
 }
