a886a935fd9b522a980179863dc74ad1f421725f
Russell V. Lenth <russell-lenth@uiowa.edu>
2014-03-23 15:56:36 +0000

    lsmeans version 2.00-3. Ordering of on.exit call. Submitted to CRAN

--- R/helpers.R
+++ R/helpers.R
@@ -189,8 +189,8 @@ lsm.basis.merMod <- function(object, trms, xlev, grid) {
     dfargs = misc = list()
     if (isLMM(object)) {
         if (requireNamespace("pbkrtest")) {
-            on.exit(detach("pbkr.ns"))
             attach(getNamespace("pbkrtest"), pos = 2, name = "pbkr.ns", warn.conflicts = FALSE)
+            on.exit(detach("pbkr.ns"))
             dfargs = list(unadjV = V, 
                 adjV = vcovAdj(object, 0))
             V = as.matrix(dfargs$adjV)
