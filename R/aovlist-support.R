366d737d1e43b2fa5c3c0a891512e1998b233a6a
Russell V. Lenth <russell-lenth@uiowa.edu>
2014-06-20 22:13:53 +0000

    lsmeans - Mods to aovlist support, implements inter-block analysis.

--- R/aovlist-support.R
+++ R/aovlist-support.R
@@ -20,36 +20,71 @@ lsm.basis.aovlist = function (object, trms, xlev, grid) {
     xnms = dimnames(X)[[2]]
     
     # Check for situations we can't handle...
-    nms = unlist(lapply(object, function(x) names(coef(x))))
-    if(max(table(match(nms, xnms))) > 1)
-        stop("Can't handle situations where the same effects appear in more than one stratum")
-    message("NOTE: 'aovlist' support is experimental.")
-    if(!all(unlist(contr) %in% c("contr.helmert","contr.poly")))
-        warning("Model uses non-orthogonal contrasts - lsmeans results probably incorrect\n",
-            "Contrast results probably OK if on one stratum (integer df)")
+    colsums = apply(X[, setdiff(xnms, "(Intercept)"), drop=FALSE], 2, sum)
+    if (any(round(colsums,3) != 0))
+        warning("Some predictors are correlated with the intercept - results are biased.\n",
+                "May help to re-fit with different contrasts, e.g. 'contr.sum'")
+    message("NOTE: 'aovlist' results use intra-block analyses and are sometimes flaky")
+    #     if(!all(unlist(contr) %in% c("contr.sum","contr.helmert","contr.poly")))
+    #         warning("Model uses non-orthogonal contrasts - lsmeans results probably incorrect\n",
+    #             "Contrast results probably OK if on one stratum (integer df)")
     
     # initialize arrays
+    nonint = setdiff(names(object), "(Intercept)")
+    
     k = length(xnms)
-    bhat = rep(0, k)
+    bhat = rep(NA, k) # I'll use NAs to track which slots I've filled
     V = matrix(0, nrow=k, ncol=k)
     names(bhat) = xnms
     dimnames(V) = list(xnms, xnms)
-    Vmats = Vidx = Vdf = list()
-    for (nm in setdiff(names(object), "(Intercept)")) {
+    empty.list = as.list(nonint)
+    names(empty.list) = nonint
+    Vmats = Vidx = Vdf = empty.list
+    wts = matrix(0, nrow = length(nonint), ncol = k)
+    dimnames(wts) = list(nonint, xnms)
+    # NOTE: At present, I just do intra-block analysis: wts are all 0 and 1
+    # Some earlier experimental code is commented-out using '#--'
+    #--bmat = wts
+    btemp = bhat #++ temp for tracking indexes
+    #++Work thru strata in reverse order
+    for (nm in rev(nonint)) {
         x = object[[nm]]
         bi = coef(x)
         bi = bi[!is.na(bi)]
-        ii = Vidx[[nm]] = match(names(bi), xnms)
-        bhat[ii] = bi
-        Vmats[[nm]] = vcov(x)
+        ii = match(names(bi), xnms)
+        #--bmat[nm,ii] = bi
+        #++ stmts for intra-block analysis
+        Vidx[[nm]] = use = setdiff(ii, which(!is.na(bhat))) #++ omit elts already filled
+        if(length(use) > 0) {
+            ii.left = seq_along(ii)[!is.na(match(ii,use))]
+            wts[nm, use] = 1
+            bhat[use] = bi[ii.left]
+            Vi = vcov(x)[ii.left,ii.left]
+            Vi[is.nan(Vi)] = 0 # replace NaNs with 0 - no est of error
+            Vmats[[nm]] = Vi
+            V[use,use] = Vi
+        }
+        else {
+            Vmats[[nm]] = matrix(0, nrow=0, ncol=0)
+        }
+        #++ end stmts for intra-block analysis
         Vdf[[nm]] = x$df
-        V[ii,ii] = Vmats[[nm]]
+        #--piv = x$qr$pivot[1L:x$rank]
+        #--wts[nm,ii[piv]] = diag(x$qr$qr)[seq_along(ii)]^2
     }
+    #--colsums = apply(wts,2,sum)
+    #--colsums[zapsmall(colsums) == 0] = 1
+    #--for (i in seq_along(nonint))
+    #--     wts[i, ] = wts[i, ] / colsums ## These are efficiencies, not weights
+    #--bmat = bmat * wts
+    #--bhat = apply(bmat, 2, sum)
+    
     x <- object[["(Intercept)"]]
     if (!is.null(x)) {
         # The intercept belongs in the 1st error stratum
         # So have to add a row and column to its covariance matrix
         bhat[1] = x$coefficients[1]
+        wts[1,1] = 1
         Vidx[[1]] = ii = c(1, Vidx[[1]])
         k = length(ii)
         vv = matrix(0, nrow=k, ncol=k)
@@ -61,11 +96,12 @@ lsm.basis.aovlist = function (object, trms, xlev, grid) {
         #dimnames(vv) = list(c(xnms[ii], xnms[ii]))
         Vmats[[1]] = vv
     }
-    dfargs = list(Vmats=Vmats, Vidx=Vidx, Vdf=unlist(Vdf))
+    dfargs = list(Vmats=Vmats, Vidx=Vidx, Vdf=unlist(Vdf), wts = wts)
     dffun = function(k, dfargs) {
-        v = sapply(1:length(dfargs$Vdf), function(j) {
+        v = sapply(seq_along(dfargs$Vdf), function(j) {
             ii = dfargs$Vidx[[j]]
-            sum(k[ii] * dfargs$Vmats[[j]] %*% k[ii])
+            kk = (k * dfargs$wts[j, ])[ii]            
+            sum(kk * dfargs$Vmats[[j]] %*% kk)
         })
         sum(v)^2 / sum(v^2 / dfargs$Vdf) # Good ole Satterthwaite
     }
