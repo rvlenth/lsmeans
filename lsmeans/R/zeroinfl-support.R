a043a5c71e88f5c488faa1d060375f6bc0638584
rlenth <rlenth@7354427b-d295-4c4f-9a55-d8da99b2380c>
2015-11-30 17:17:38 +0000

    lsmeans - fixed error in hurdle models for geometric cases

--- lsmeans/R/zeroinfl-support.R
+++ lsmeans/R/zeroinfl-support.R
@@ -103,7 +103,7 @@ lsm.basis.hurdle = function(object, trms, xlev, grid,
         p1 = switch(object$dist$count,
                 poisson = 1 - exp(-mu1),
                 negbin = 1 - (theta1 / (mu1 + theta1))^theta1,
-                geometric = 1 / (1 + mu1)  )
+                geometric = 1 - 1 / (1 + mu1)  )
         dp1 = switch(object$dist$count,
                 poisson = mu1 * exp(-mu1),
                 negbin = mu1 * (theta1 / (mu1 + theta1))^(1 + theta1),
@@ -123,12 +123,12 @@ lsm.basis.hurdle = function(object, trms, xlev, grid,
                 binomial = mu2,
                 poisson = 1 - exp(-mu2),
                 negbin = 1 - (theta2 / (mu2 + theta2))^theta2,
-                geometric = 1 / (1 + mu2)  )
+                geometric = 1 - 1 / (1 + mu2)  )
         dp2 = switch(object$dist$zero,
                 binomial = stats::make.link(object$link)$mu.eta(lp2),
                 poisson = mu2 * exp(-mu2),
                 negbin = mu2 * (theta2 /(mu2 + theta2))^(1 + theta2),
-                geometric = 1 / (1 + mu2)^2)
+                geometric = mu2 / (1 + mu2)^2  )
 
         if (mode == "mean") {
             bhat = p2 * mu1 / p1
