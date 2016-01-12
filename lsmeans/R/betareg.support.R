1b2bf2faf75b5fd4d9af549f4f19298f515b8eb5
rlenth <rlenth@7354427b-d295-4c4f-9a55-d8da99b2380c>
2016-01-12 23:35:32 +0000

    lsmeans - more support for 'betareg'

--- lsmeans/R/betareg.support.R
+++ lsmeans/R/betareg.support.R
@@ -1,31 +1,50 @@
 # Support for 'betareg' class
 
-# NEED TO FIX THIS for precision-related modes
-recover.data.betareg = function(object, ...) {
+# mode is same as 'type' in predict.betareg, PLUS 
+# mode = "phi.link" refers to link function before back-transforming to "precision"
+
+recover.data.betareg = function(object, mode = c("response", "link", "precision", "phi.link", "variance", "quantile"), ...) {
     fcall = object$call
-    recover.data(fcall, delete.response(terms(object)), object$na.action, ...)
+    mode = match.arg(mode)
+    if (mode  %in% c("response", "link"))
+        mode = "mean"
+    if (mode == "phi.link") 
+        mode = "precision"
+    if(mode %in% c("response", "link", "precision"))
+        trms = delete.response(terms(object, model = mode))
+    else
+        trms = delete.response(object$terms$full)
+    # Make sure there's an offset function available
+    env = new.env(parent = attr(trms, ".Environment"))
+    env$offset = function(x) x
+    attr(trms, ".Environment") = env
+    recover.data(fcall, trms, object$na.action, ...)
 }
 
 # PRELIMINARY...
-# Currently works correctly only for "resp" and "link" modes
+# Currently works correctly only for "resp", "link", "precision", "phi" modes
 lsm.basis.betareg = function(object, trms, xlev, grid, 
-        mode = c("response", "link", "precision", "variance", "quantile"), 
+        mode = c("response", "link", "precision", "phi.link", "variance", "quantile"), 
         quantile = .5, ...) {
     mode = match.arg(mode)
-    if (mode == "quantile")
-        stop('"quantile" mode is not yet supported.')
+    if (mode %in% c("variance", "quantile"))
+        stop(paste0('"', mode, '" mode is not yet supported.'))
     
-    model = ifelse(mode %in% c("response", "link"), "mean", "precision")
+    # figure out which parameters we need
+    model = if (mode %in% c("response", "link")) "mean"
+        else if (mode %in% c("precision", "phi.link")) "precision"
+        else "full"
+    V = vcov(object, model = model)
+    bhat = coef(object, model = model)
     
     m = model.frame(trms, grid, na.action = na.pass, xlev = xlev)
-    X = model.matrix(trms, m, contrasts.arg = object$contrasts$mean)
-    bhat = coef(object, model = model) 
-    V = vcov(object, model = model)
+    X = model.matrix(trms, m, contrasts.arg = object$contrasts[[model]])
+    
     nbasis = estimability::all.estble
     misc = list(tran = object$link[[model]]$name)
     dffun = function(k, dfargs) NA
     dfargs = list()
-    if (mode %in% c("response", "variance")) {
+    if (mode %in% c("response", "precision")) {
         misc$postGridHook = ".betareg.pg"
     }
     list(X=X, bhat=bhat, nbasis=nbasis, V=V, dffun=dffun, dfargs=dfargs, misc=misc)
