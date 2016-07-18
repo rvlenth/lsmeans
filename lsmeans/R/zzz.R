38a331a1b6cf536940d6fd036310e48396db2947
rlenth <rlenth@7354427b-d295-4c4f-9a55-d8da99b2380c>
2016-07-18 19:46:24 +0000

    Fixes to hasName. Bug-fix version 2.23-5 went to CRAN

--- lsmeans/R/zzz.R
+++ lsmeans/R/zzz.R
@@ -1,11 +1,16 @@
 # Namespace hooks for lsmeans
 
-.onLoad = function(libname, pkgname) {
-    # Function to check for existence of a variable
-    # This will be in the base package of R > 3.3.0
-    if (!exists("hasName", envir = getNamespace("utils"), inherits = FALSE)) {
-        assign("hasName", function(x, name)
-                match(name, names(x), nomatch = 0L) > 0L,
-            envir = getNamespace(pkgname))
-    }
-}
+# .onLoad = function(libname, pkgname) {
+#     # Function to check for existence of a variable
+#     # This will be in the base package of R > 3.3.0
+#     if (!exists("hasName", envir = getNamespace("utils"), inherits = FALSE)) {
+#         assign("hasName", function(x, name)
+#                 match(name, names(x), nomatch = 0L) > 0L,
+#             envir = getNamespace(pkgname))
+#     }
+# }
+
+# Just define the function for now. Maybe in a year we require R >= 3.4
+hasName = function(x, name)
+    match(name, names(x), nomatch = 0L) > 0L
+
