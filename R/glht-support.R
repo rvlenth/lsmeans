7a7a7a32eb47fa04471ed036fa96445e0d9406e3
Russell V. Lenth <russell-lenth@uiowa.edu>
2016-11-16 01:14:16 +0000

    lsmeans - License to GPL-2|GPL-3 and added copyright etc to files

--- R/glht-support.R
+++ R/glht-support.R
@@ -1,3 +1,24 @@
+##############################################################################
+#    Copyright (c) 2012-2016 Russell V. Lenth                                #
+#                                                                            #
+#    This file is part of the lsmeans package for R (*lsmeans*)              #
+#                                                                            #
+#    *lsmeans* is free software: you can redistribute it and/or modify       #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 2 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    *lsmeans* is distributed in the hope that it will be useful,            #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU General Public License for more details.                            #
+#                                                                            #
+#    You should have received a copy of the GNU General Public License       #
+#    along with R and *lsmeans*.  If not, see                                #
+#    <https://www.r-project.org/Licenses/> and/or                            #
+#    <http://www.gnu.org/licenses/>.                                         #
+##############################################################################
+
 ### Code for an enhancement of 'glht' in 'multcomp' package
 ### Provides for using 'lsm' in similar way to 'mcp'
 ### This is implemented via the class "lsmlf" -- linear functions for lsmeans
