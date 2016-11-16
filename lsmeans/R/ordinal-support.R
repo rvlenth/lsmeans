c942ed2748443c127df5553764e0ea930499793e
rlenth <rlenth@7354427b-d295-4c4f-9a55-d8da99b2380c>
2016-11-16 01:14:16 +0000

    lsmeans - License to GPL-2|GPL-3 and added copyright etc to files

--- lsmeans/R/ordinal-support.R
+++ lsmeans/R/ordinal-support.R
@@ -1,6 +1,25 @@
-#--------------------------------------------------------------
+##############################################################################
+#    Copyright (c) 2012-2016 Russell V. Lenth                                #
+#                                                                            #
+#    This file is part of the lsmeans package for R (*lsmeans*)              #
+#                                                                            #
+#    *lsmeans* is free software: you can redistribute it and/or modify       #
+#    it under the terms of the GNU General Public License as published by    #
+#    the Free Software Foundation, either version 2 of the License, or       #
+#    (at your option) any later version.                                     #
+#                                                                            #
+#    *lsmeans* is distributed in the hope that it will be useful,            #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#    GNU General Public License for more details.                            #
+#                                                                            #
+#    You should have received a copy of the GNU General Public License       #
+#    along with R and *lsmeans*.  If not, see                                #
+#    <https://www.r-project.org/Licenses/> and/or                            #
+#    <http://www.gnu.org/licenses/>.                                         #
+##############################################################################
+
 ### support for the ordinal package
-# (initial setup thanks to Maxime Herve, RVAideMemoire package)
 
 recover.data.clm = function(object, mode = "latent", ...) {
     if (!is.na(pmatch(mode, "scale"))) {
